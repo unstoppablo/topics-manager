e7ed7fc311cb1d0ef304f057919fd1cb0c2c7432
Pablo Labbate <66883135+unstoppablo@users.noreply.github.com>
2023-07-26 18:06:06 +0000

    made changes to code

--- add_topics.py
+++ add_topics.py
@@ -15,63 +15,72 @@ token = os.getenv("PL_PAT")
 access = "Bearer " + token
 headers = { "Accept": "application/vnd.github+json", "Authorization": access }
 
-def open_file(path):
-    with open(path, 'r') as file:
-        data = json.load(file)
-    
-    repo_dict = {}
-    
-    for entry in data:
-        name = entry["name"]
+def check_str(myStr):
+    open = "["
+    close = "]"
+    stack = []
+    for i in myStr:   # check each character in the string for a [ or a ]
+        if i == open: 
+            stack.append(i) # if character equals [ add to stack list
+        elif i == close:    # check previous character for [
+            if len(stack) > 0 and open == stack[len(stack)-1]:
+                stack.pop() # remove characters from stack list
+            else: # if unable to find [ in stack list then brackets are not contained
+                print("Items are not contained within the brackets")
+                sys.exit(1)
+    if len(stack) == 0:
+        return 
+    else: # if [ is still in list then items are not contained
+        print("Items are not contained within the brackets")
+        sys.exit(1)	    
 
-        owner = entry["owner"]["login"]  # owner is a dict
-        topics = entry["topics"]
-        repo_dict[name] = (owner, topics)
-    
+
+def get_repo_and_topics_from_args():
+    if len(sys.argv) < 2:
+        print("Usage: add_topics.py [<repo_name>, <topic1>, <topic2] [<other_repo_name>, <topic1>, <topic2] ")  # Changing the format to have parenthesis and commas
+        sys.exit(1)
+    arg = " ".join(sys.argv[1:])  # put everything after add_topics.py into a string
+    bracket_only = bool(re.search(r"[({})]", arg)) # verifying that only brackets are being used
+    if bracket_only:
+        print("Please only use bracket")
+        sys.exit(1)
+    check_str(arg)
+    arg = arg.replace("[", "")  # remove ( from string
+    args_list = arg.split("]")  # use ) as a delimiter for each repository
+
+    #  [<repo_name>, <topic1>, <topic2] -->  ["<repo_name>","<topic1>","<topic2"]
+    args_list = [element.strip() for element in args_list if element.strip()]  # strip spaces and filter any empty strings
+    print("printing arg after split", args_list)
+    repo_dict = {}  # where we store repos and their topics
+    for i in args_list:
+        i_list = i.split(",")  # split string into list by ,
+        i_list = [element.strip() for element in i_list]  # strip spaces
+        repo_key = i_list.pop(0)  # store the first value of i_list
+        print("Repo Name: " + repo_key)
+        repo_dict[repo_key] = []
+        
+        for topic in i_list:
+            repo_dict[repo_key].append(topic) # add topics to repo name dictionary if it's in repo dictionary
     return repo_dict
 
+def get_existing_topics(target_repo):
+    topics_url = gh_contributors_url + target_repo + "/topics"
+    topics_resp = session.get(topics_url, headers=headers)
+    return topics_resp
 
-def get_user_input(repos_dict):
-    target_repo = input("Enter the target repo: ")
-    topics = []
-
-    if target_repo in repos_dict:
-        while True:
-            topic = input("Enter a topic (press Enter to finish): ")
-            if topic == "":
-                break
-            topics.append(topic)
-        
-        return target_repo, topics
-    else:
-        # entered invalid repo name
-        return -1, -1
-
-def get_repo_and_topics_from_args(repos_dict):
-    if len(sys.argv) < 2:
-        print("Usage: add_topics.py <repo_name> <topic1> [<topic2> ...]")
-        sys.exit(1)
-    
-    repo_name = sys.argv[1]
-
-    if repo_name in repos_dict:
-
-        topics = sys.argv[2:]
-        
-        if not topics:
-            print("Error: At least one topic must be provided.")
-            sys.exit(1)
-        
-        return repo_name, topics
-    else:
-        return -1, -1
-
-def add_topics(repo, new_topics, existing_topics, repo_owner):
+def add_topics(repo, new_topics, existing_topics):
     existing_topics = set(existing_topics) # changing to set to avoid duplicates
-    for topic in new_topics:
-        existing_topics.add(topic)
-
-    existing_topics = list(existing_topics)
+    if len(new_topics) != 0:
+        for topic in new_topics:
+            if bool(re.search("[a-z]", topic)) == False: # check if topics only contain lowercase letters
+                    print("Topics must be in all lowercase")
+                    sys.exit(1)
+            else:
+                existing_topics.add(topic)
+    else:
+        print("unable to add an empty topic")
+        sys.exit(1)
+    existing_topics = list(existing_topics) # converting set back into a list
     if len(existing_topics) > 0:
         data = {
             "names": existing_topics
@@ -81,56 +90,31 @@ def add_topics(repo, new_topics, existing_topics, repo_owner):
     else:
         print("0 topics provided. Quitting.")
         sys.exit(1)
-        
-
-def test_obtained(target_repo):
-    topics_url = gh_contributors_url + target_repo + "/topics"
-    topics_resp = session.get(topics_url, headers=headers)
-    return topics_resp
 
 if __name__ == "__main__":
-    repo_dict = open_file("cloud_data.json")
-
-    target_repo, topics = get_repo_and_topics_from_args(repo_dict)
-    # target_repo, topics = get_user_input(repo_dict)
-
-    if target_repo != -1:
-        print("You wish to add the following topics to: ", target_repo)
-        print(topics)
-        
-        existing_topics = repo_dict[target_repo][1]  # returns existing topics
-        
-        repo_owner = repo_dict[target_repo][0]
-        add_topics_resp = add_topics(target_repo, topics, existing_topics['names'], repo_owner)
-        print("Attempted adding topics, resp code: ")
-        print(add_topics_resp)
-        
-        topics_resp = test_obtained(target_repo)
-        if topics_resp.status_code == 200:
-            print("Topics after being added: ")
-            print(topics_resp.json())
-        else:
-            print("Error obtaining latest topics for repo: ", target_repo)
-            print("Error code: ", topics_resp)
-    else:
-        print("Repo could not be found.")
-        
-    # while True:
-    #     target_repo, topics = get_user_input(repo_dict)
-
-    #     if target_repo != -1:
-    #         print("You wish to add the following topics to: ", target_repo)
-    #         print(topics)
-    #         choice = input("Do you wish to proceed? (Y/N)")
-    #         if choice == "Y":
-    #             existing_topics = repo_dict[target_repo][1]  # returns existing topics
-    #             repo_owner = repo_dict[target_repo][0]
-    #             add_topics(target_repo, topics, repo_dict, existing_topics, repo_owner)
-    #         else:
-    #             cancel = input("Restarting. Enter 'quit' to abort program.")
-    #             if cancel == "quit":
-    #                 break
-    #     else:
-    #         cancel = input("Repo could not be found, please enter 'quit' to abort or press anything else to continue.")
-    #         if cancel == "quit":
-    #                 break
+    new_repo_dict = get_repo_and_topics_from_args()
+    
+    for target_repo, topics in new_repo_dict.items():
+        try:
+            print("You wish to add the following topics to: ", target_repo)
+            print(topics)
+            
+            topics_response = get_existing_topics(target_repo)  # returns existing topics
+            if topics_resp.status_code == 200:
+                existing_topics = topics_resp.json()['names']  # returns list of topics
+            else:
+                existing_topics = []
+            
+            add_topics_resp = add_topics(target_repo, topics, existing_topics['names'], )
+            print("Attempted adding topics, resp code: ")
+            print(add_topics_resp)
+            
+            topics_resp = get_existing_topics(target_repo)
+            if topics_resp.status_code == 200:
+                print("Topics after being added: ")
+                print(topics_resp.json())
+            else:
+                print("Error obtaining latest topics for repo: ", target_repo)
+                print("Error code: ", topics_resp)
+        except Exception as e:
+            print("Error during adding topic to repo process: ", e)
